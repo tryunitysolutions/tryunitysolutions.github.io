8a80f8c3a7363b13b5158cd7d6053c93a7ab4d50
tryunitysolutions <infotryunity@gmail.com>
2025-06-14 15:31:05 +0500

    update vite config file for path error

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/try-unity-solutions/', // ✅ This fixes 404 errors on GitHub Pages
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
-                    }
-                }
-            }
+  base: '/', // ✅ important for tryunitysolutions.github.io repo
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('swiper')) return 'swiper';
+          }
         }
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+      },
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    },
+  },
 })
